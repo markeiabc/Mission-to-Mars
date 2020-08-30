fe1dc68606461365ae59fb5120df909b50844232
Markeia Brox-Chester <markeia@markeias-mbp.lan>
2020-08-29 18:56:55 -0700

    updating module work post scrape

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template
 from flask_pymongo import PyMongo
 import scraping
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='template')
 
 # Use flask_pymongo to set up mongo connection
 app.config["MONGO_URI"] = "mongodb://localhost:27017/mars_app"
