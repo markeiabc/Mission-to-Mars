6f08e57321488923442965918c3156ccb1f606f2
Markeia Brox-Chester <markeia@markeias-mbp.lan>
2020-08-29 23:12:31 -0700

    Module 10 Challenge Complete

--- scraping.py
+++ scraping.py
@@ -21,6 +21,7 @@ def scrape_all():
         "news_paragraph": news_paragraph,
         "featured_image": featured_image(browser),
         "facts": mars_facts(),
+        "hemisphere_image": hemisphere_image(browser),
         "last_modified": dt.datetime.now()
     }
 
@@ -104,8 +105,49 @@ def mars_facts():
     # Convert dataframe into HTML format, add bootstrap
     return df.to_html(classes="table table-striped")
 
+# Module 10 Challenge
+def hemisphere_image(browser):
+# Visit URL
+    url = "https://astrogeology.usgs.gov/search/results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    browser.visit(url)
+
+    hemisphere_image_urls = []
+    links = browser.find_by_css("a.product-item h3")
+
+    for index in range(len(links)):
+        hemisphere = {}
+        browser.find_by_css("a.product-item h3")[index].click()
+        sample_element = browser.links.find_by_text ("Sample").first
+        hemisphere["img_url"] = sample_element["href"]
+        hemisphere["title"] = browser.find_by_css("h2.title").text
+        hemisphere_image_urls.append(hemisphere)
+        
+        browser.back()
+    return hemisphere_image_urls
+
+def scrape_hemisphere(html_text):    
+    #parse html text
+    hemispheres_soup= soup(html_text, "html.parser")
+
+    try:
+        title_element = hemispheres_soup.find("h2", class_= "title").get_text()
+        sample_element= hemispheres_soup.find("a", text = "Sample").get("hred")
+    except AttributeError:
+        title_element = None
+        sample_element = None
+
+    hemispheres_dictionary = {
+        "title": title_element,
+        "img_url": sample_element
+    }
+    return hemispheres_dictionary
+
 if __name__ == "__main__":
 
     # If running as script, print scraped data
     print(scrape_all())
+
+
+
+
 
